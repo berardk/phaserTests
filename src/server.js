82de8d4a224633abf7f6001ee95a5ff4d6706144
Reeker <Reeker@DESKTOP-RFCG9SO>
2017-03-18 18:57:32 +0100

    KBE fix port error heroku

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const express = require('express');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = express()
 .use(express.static(__dirname + '/public'))
